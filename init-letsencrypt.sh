bcb10470c4933f7d791a7606bd8957dea1e1e2c8
Amit Wani <mtwn105@gmail.com>
2022-06-08 20:09:51 +0530

    Lets Encrypt and SSL changes

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -39,7 +39,7 @@ echo
 
 
 echo "### Starting nginx ..."
-docker-compose up --force-recreate -d nginx
+docker-compose up --force-recreate -d whatsapp-connector nginx
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
